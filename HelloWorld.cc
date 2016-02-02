a3c73f8666f3ffbd1b3115ef2bb6a9bbf06d7ac0
Emma J. Franza <emmafranza@storm.cis.fordham.edu>
2016-02-01 20:32:48 -0500

    Hello World!

---
+++ HelloWorld.cc
@@ -0,0 +1,16 @@
+// cin with strings
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+	string a;
+	cin >> a;
+
+	string b;
+	cin >> b;
+
+	cout << "Hello " << a << endl;
+	cout << "Hello " << b << endl;
+}
