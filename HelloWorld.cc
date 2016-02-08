5724e2cd60056dc403deb713d86a745df51fe3f1
Emma J. Franza <emmafranza@storm.cis.fordham.edu>
2016-02-08 18:45:35 -0500

    Updated

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -7,15 +7,15 @@ int main ()
 {
 	string username;
 	cout << "Enter your name: ";
-	cin >> username;
+	getline(cin, username);
 	
 	string userage;
 	cout<< "Enter your age: ";
-	cin >> userage;
+	getline(cin, userage);
 
 	string userthing;
 	cout<< "Enter your favorite thing: ";
-	cin >> userthing;
+	getline(cin, userthing);
 
 	cout << "Hi, my name is " << username << "." << " I am " << userage << " years old and my favorite thing is " << userthing << "." << endl;
 	return 0;
