380b0743edabbf25ee5eb7e32eed8edfb51404f3
Emma J. Franza <emmafranza@storm.cis.fordham.edu>
2016-02-16 19:30:33 -0500

    Assignment 4

---
+++ Hw4.cc
@@ -0,0 +1,16 @@
+//Using loops, have the user input three numbers and then add them all together;
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+	int x = i;
+	while(i < 3)  
+	{
+	cout << "Input a number: ";
+
+	if(i==3) break;
+	cout << "The summation of the above numbers is: " << x+x+x << endl;
+	}
+	return 0;
+}
