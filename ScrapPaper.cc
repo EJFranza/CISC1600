ff6b7229e5b146b03f95e5e6219cdc8b2db03766
Emma J. Franza <emmafranza@storm.cis.fordham.edu>
2016-02-16 19:43:58 -0500

    Updated Programme

--- ScrapPaper.cc
+++ ScrapPaper.cc
@@ -4,6 +4,13 @@ using namespace std;
 
 int main ()
 {
-
+	string username;
+	cout << "Hello there, what is your name?" <<endl;
+	getline (cin, username);
+	cout << "It is very nice to meet you, " << username << endl;
+	cout << "Forgive me for asking, " << username << ". But are you a boy or a girl?" << endl;
+	
+	string gender;
+	
 return 0;
 }
