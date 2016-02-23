2d16cce3653a668ed494f40bade69875a16cc8a4
Emma J. Franza <emmafranza@storm.cis.fordham.edu>
2016-02-22 19:19:36 -0500

    Assignment 5

---
+++ Hw5.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+	int number = 0;
+	int y = 5;
+ 
+	cout << "Let's play a game! I am thinking of a number. Try to guess it!"<< endl;
+	cin >> number;
+	while (number != y)
+	{
+		if ( number < y )
+		{
+			cout<< "Oops, too small! Guess again: "<< number << endl;
+			cin >> number;
+		}		
+		else if (number > y )
+		{
+			cout << "Oops, too big! Guess again: " << number << endl;
+			cin >> number;
+		}
+	}
+		cout << "Bingo! The number you guessed is correct." << endl;
+	return 0;
+}
